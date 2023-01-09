e1808df3fa3636c05f969670c5628c07b8153139
melekmoalla <melekmoalla2468@gmail.com>
2023-01-09 10:50:31 +0100

    test code

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    if (parent == NULL)
+        return (NULL);
+
+    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+    if (new_node == NULL)
+    {
+        return (NULL);
+    }
+    new_node->n = value;
+    new_node->right = NULL;
+    new_node->left = parent->left;
+    if (new_node->left != NULL)
+    {
+        new_node->left->parent = new_node;
+    }
+    parent->left = new_node;
+    new_node->parent = parent;
+
+    return (new_node);
+}
