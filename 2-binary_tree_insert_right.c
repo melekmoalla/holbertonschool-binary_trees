b7eb574024f9974bbd38481399925ed289f70a36
melekmoalla <melekmoalla2468@gmail.com>
2023-01-09 11:23:17 +0100

    test code

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -29,5 +29,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		new_node->right->parent = new_node;
 	}
 	parent->right = new_node;
-	return (parent);
+	return (new_node);
 }
