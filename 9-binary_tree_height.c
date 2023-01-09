4b2eb3ae5fe1cf1f37795463a9c2251d0d3dcda0
melekmoalla <melekmoalla2468@gmail.com>
2023-01-09 20:45:33 +0100

    test code hhhh after hard working i guess this code that i always modify it id true hhhhh go in this way

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,20 +8,25 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t depth = -1;
+	
 
-	while (tree)
+	if (!tree)
 	{
-		if (tree->left != NULL)
-		{
-			tree = tree->left;
-		}
-		else
-		{
-			tree = tree->right;
-		}
-		depth++;
+		return 0;
 	}
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-	return (depth);
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+	{
+		return left_height + 1;
+	}
+	else
+	{
+		return right_height + 1;
+	}
 }
+
