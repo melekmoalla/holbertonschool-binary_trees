398eb2f5c967571efa3a17ea1a528fc47195ff51
melekmoalla <melekmoalla2468@gmail.com>
2023-01-09 14:23:19 +0100

    test code && fix betty

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf -  function that checks if a node is a leaf
+ * @node: is a pointer to the node to check
+ * Return:  function must return 1 if node is a root, otherwise 0
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+    if (node == NULL)
+    {
+        return (0);
+    }
+    if (node->right != NULL && node->left != NULL)
+    {
+        return (1);
+    }
+    return (0);
+}
