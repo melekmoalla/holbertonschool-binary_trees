dce61a9b8860da1c202a4a5f85df5502d3a7e6fe
melekmoalla <melekmoalla2468@gmail.com>
2023-01-10 00:40:18 +0100

    correct

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,20 +8,18 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t a, b;
+
 	if (tree == NULL)
-	{
 		return (0);
-	}
+
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	if (tree->left != NULL && tree->right != NULL && tree->parent == NULL)
-	{
-		return (2);
-	}
-	if (tree->left != NULL && tree->right != NULL && tree->parent != NULL)
-	{
-		return (2);
-	}
+
 	else
-		return (1);
+	{
+		a = binary_tree_leaves(tree->left);
+		b = binary_tree_leaves(tree->right);
+	}
+	return (a + b + 1);
 }
