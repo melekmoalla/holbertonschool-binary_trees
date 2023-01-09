6b732a9a2ec0f99f6b94a678ce12258f35cf34bc
melekmoalla <melekmoalla2468@gmail.com>
2023-01-09 12:03:49 +0100

    test code

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - function that inserts a node as the left-child
+ * @parent:  is a pointer to the parent node of the node to create
+ * @value:is the value to put in the new node
+ * Return: new_node or NULL if failure.
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (tree == NULL)
+    {
+        return;
+    }
+
+    binary_tree_t *current = tree;
+    binary_tree_t *next;
+    
+    while (current != NULL)
+    {
+        if (current->left != NULL)
+        {
+            next = current->left;
+            current->left = NULL;
+        }
+        else if (current->right != NULL)
+        {
+            next = current->right;
+            current->right = NULL;
+        }
+        else
+        {
+            next = NULL;
+        }
+        free(current);
+        current = next;
+    }
+}
