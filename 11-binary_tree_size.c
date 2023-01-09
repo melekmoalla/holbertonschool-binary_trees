674688460c9ca89566c8a1036ac064c160b2dc0b
melekmoalla <melekmoalla2468@gmail.com>
2023-01-09 22:15:27 +0100

    test correct and fix betty hhh gooo in this way hhhh to go this route to succeed
    3

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - function that measures the size of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the height.
+ * Return: size of a binary tree of 0 if is tree == null
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+
+	if (tree == NULL)
+		return (0);
+	else
+		return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
