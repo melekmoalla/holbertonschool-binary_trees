e2202a2dfeb972445b4bd38ca4b36b52a34c25c4
melekmoalla <melekmoalla2468@gmail.com>
2023-01-10 14:18:00 +0100

    test code 14 14v14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -39,13 +39,13 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	a = binary_tree_height(tree->left);
 	b = binary_tree_height(tree->right);
-	if (a - b == 1 && (tree->left != NULL && tree->right != NULL))
+	if (a - b >  1)
 	{
-		return (a - b);
+		return (a - b + 1);
 	}
 	if (a - b < -1)
 	{
 		return (a - b - 1);
 	}
-	return (a - b + 1);
+	return (a - b);
 }
