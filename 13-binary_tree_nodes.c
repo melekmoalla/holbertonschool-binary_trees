929a7e6754e39a23aadc2085d7d8861e8f8967a2
melekmoalla <melekmoalla2468@gmail.com>
2023-01-10 01:11:06 +0100

    test code && fix betty

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,46 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the height.
+ * Return: counts the leaves in a binary tree
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t a = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+	while (tree)
+	{
+		a++;
+		if (tree->left == NULL && tree->right == NULL)
+		{
+			break;
+		}
+		if (tree->left == NULL && tree->right != NULL)
+		{
+			tree = tree->right;
+		}
+		else if (tree->left != NULL && tree->right == NULL)
+		{
+			tree = tree->left;
+		}
+		else if (tree->left->n > tree->right->n)
+		{
+			tree = tree->left;
+		}
+		else
+		{
+			tree = tree->right;
+		}
+	}
+	return (a);
+}
