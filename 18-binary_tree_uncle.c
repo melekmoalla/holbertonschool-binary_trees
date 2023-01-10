0d081a687c54d7976f6e4ddfd402aa85acef4479
melekmoalla <melekmoalla2468@gmail.com>
2023-01-10 20:17:12 +0100

    test and fix betty I thing it's ok is true

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle -  function that finds the uncle of a node
+ * @node: is a pointer to the root node of the tree to measure the height.
+ * Return: the number of the children
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node->parent == node->parent->parent->left)
+	{
+		return (node->parent->parent->right);
+	}
+	else
+	{
+		return (node->parent->parent->left);
+	}
+}
