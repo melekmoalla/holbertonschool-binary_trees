5113db5100efba41e160984c94f45a1f65c5c4d6
melekmoalla <melekmoalla2468@gmail.com>
2023-01-10 14:55:53 +0100

    test correct return

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -46,10 +46,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
     left_height = binary_tree_height(tree->left);
     right_height = binary_tree_height(tree->right);
 
-
-    if (left_height != right_height)
+    if (left_height == right_height)
     {
-        return (0);
+        return (1);
     }
-    return (1);
+    return (0);
 }
