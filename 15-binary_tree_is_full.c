e5f210264c6e547463353f2cc55c71e49a23e043
melekmoalla <melekmoalla2468@gmail.com>
2023-01-10 15:48:51 +0100

    correct

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,19 +8,18 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    // If empty tree
+
     if (tree == NULL)
         return 0;
 
-    // If leaf node
+
     if (tree->left == NULL && tree->right == NULL)
         return 1;
 
-    // If both left and right are not NULL, and left & right subtrees
-    // are full
+
     if ((tree->left) && (tree->right))
         return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
-    // We reach here when none of the above if conditions work
+
     return (0);
 }
