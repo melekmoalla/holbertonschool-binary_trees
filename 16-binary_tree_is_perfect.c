6878675756ecb158e970ad2a3e5f227e0990edf7
melekmoalla <melekmoalla2468@gmail.com>
2023-01-10 19:19:26 +0100

    fix betty and add globale variable

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-int a = 0;
+
 
 /**
  * binary_tree_height -  function that measures the height of a binary tree
@@ -38,6 +38,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  * @tree: is a pointer to the root node of the tree to measure the height.
  * Return: 0 or 1
  */
+
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
--- binary_trees.h
+++ binary_trees.h
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int a = 0;
+
 /**
  * struct binary_tree_s - Binary tree node
  *
