09a7a6ce9cdd34c967b0c1f9f0ec45728927a06d
melekmoalla <melekmoalla2468@gmail.com>
2023-01-13 00:07:45 +0100

    test some thing

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,7 +8,7 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height;
+
 	size_t right_height;
 
 	if (!tree)
@@ -18,7 +18,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
+	binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
 	return (right_height + 1);
