1b2eabc3f740d6364177cdbee5c81464f8bd29f5
melekmoalla <melekmoalla2468@gmail.com>
2023-01-13 00:33:50 +0100

    correct

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,7 +8,7 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-
+	size_t left_height;
 	size_t right_height;
 
 	if (!tree)
@@ -18,8 +18,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	binary_tree_height(tree->left);
+	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	return (right_height + 1);
+	if (left_height >= right_height)
+	{
+		return (left_height + 1);
+	}
+	else
+	{
+		return (right_height + 1);
+	}
 }
