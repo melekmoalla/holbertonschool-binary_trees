7eec0b495655d7446fbb8433496f693b3a8e1c4d
melekmoalla <melekmoalla2468@gmail.com>
2023-01-13 01:57:17 +0100

    test some

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -16,7 +16,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	}
 	if (tree->left == NULL && tree->right == NULL)
-		return (1);
+		return (0);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
