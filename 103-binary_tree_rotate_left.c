fe6a774c3fcc25901e5ca7e6197dc49c0675b51a
melekmoalla <melekmoalla2468@gmail.com>
2023-01-13 12:37:55 +0100

    test code fix betty

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,10 +8,10 @@
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
+	binary_tree_t *new_root = tree->right;
+
 	if (tree == NULL || tree->right == NULL)
 		return (NULL);
-
-	binary_tree_t *new_root = tree->right;
 
 	if (new_root->left == NULL)
 	{
